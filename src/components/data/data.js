76dad13ea334e43ac9529a130b4e8fad59e06964
DireStraits01 <arsamak88@gmail.com>
2023-01-20 02:07:18 +0300

    add modal bottstrap react

---
+++ src/components/data/data.js
@@ -0,0 +1,57 @@
+let pizzaObjData = [
+  {
+    img: '/img/cheese.png',
+    title: 'Margarita',
+    ingredients:
+      'Baked pork ham, cranberry sauce, lingonberries, red onion, mozzarella, signature alfredo sauce',
+    price: 23,
+  },
+  {
+    img: '/img/chiken-burger.png',
+    title: 'Chicken-burger',
+    ingredients:
+      'Bacon, Chicken, Onion, Pickled cucumbers, Fresh tomatoes, Mozzarella cheese, Piquant sauce',
+    price: 20,
+  },
+  {
+    img: '/img/4season.png',
+    title: '4 Season',
+    ingredients:
+      'Bacon, Ham, Mushrooms, Chicken, Onions, Olives, Pickled Cucumbers, Hunting sausages, Pepperoni, Fresh tomatoes, BBQ sauce, Mozzarella cheese, Feta cheese, Tomato sauce',
+    price: 18,
+  },
+  {
+    img: '/img/pepperoni.png',
+    title: 'Pepperoni',
+    ingredients: 'Pepperoni, Mozzarella Cheese, Tomato sauce',
+    price: 21,
+  },
+  {
+    img: '/img/4season.png',
+    title: '4 Season',
+    ingredients:
+      'Bacon, Ham, Mushrooms, Chicken, Onions, Olives, Pickled Cucumbers, Hunting sausages, Pepperoni, Fresh tomatoes, BBQ sauce, Mozzarella cheese, Feta cheese, Tomato sauce',
+    price: 18,
+  },
+  {
+    img: '/img/pepperoni.png',
+    title: 'Pepperoni',
+    ingredients: 'Pepperoni, Mozzarella Cheese, Tomato sauce',
+    price: 21,
+  },
+  {
+    img: '/img/4season.png',
+    title: '4 Season',
+    ingredients:
+      'Bacon, Ham, Mushrooms, Chicken, Onions, Olives, Pickled Cucumbers, Hunting sausages, Pepperoni, Fresh tomatoes, BBQ sauce, Mozzarella cheese, Feta cheese, Tomato sauce',
+    price: 18,
+  },
+  {
+    img: '/img/pepperoni.png',
+    title: 'Pepperoni',
+    ingredients: 'Pepperoni, Mozzarella Cheese, Tomato sauce',
+    price: 21,
+  },
+];
+
+export default pizzaObjData;
--- src/components/pages/Pizza.jsx
+++ src/components/pages/Pizza.jsx
@@ -1,22 +1,51 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import style from './Pizza.module.css';
-import pz from './img/cheese.png';
+import pizzaObjData from '../data/data';
+
 function Pizza() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <div className={style.row}>
-        <div className={style.pizzaCard}>
-          <img src={pz} alt="" />
-          <h3>Margarita</h3>
-          <p>
-            Запеченная буженина из свинины, клюквенный соус, брусника, красный
-            лук, моцарелла, фирменный соус альфредо
-          </p>
-          <div className={style.pizzaCard__footer}>
-            <span className={style.pizzaCard__price}>$23</span>
-            <button>add</button>
-          </div>
-        </div>
+        {pizzaObjData.map((pizzas, index) => {
+          return (
+            <div className={style.pizzaCard} key={index}>
+              <img src={pizzas.img} alt="" onClick={handleShow} />
+              <h3>{pizzas.title}</h3>
+              <p>{pizzas.ingredients}</p>
+              <div className={style.pizzaCard__footer}>
+                <span className={style.pizzaCard__price}>${pizzas.price}</span>
+                <Button variant="primary" onClick={handleShow}>
+                  show
+                </Button>
+              </div>
+            </div>
+          );
+        })}
       </div>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton className="border-0">
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body></Modal.Body>
+        <Modal.Footer className="border-0">
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
