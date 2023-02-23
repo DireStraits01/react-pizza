c2fb70b1607563a0bac9b2dfd77535a5384c6d7c
DireStraits01 <arsamak88@gmail.com>
2023-02-23 20:06:07 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
           cartCount={cartCount}
           setCartCount={setCartCount}
         />
+
         <Routes>
           <Route
             index
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,57 @@ function Footer() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum illum
           </p>
         </div>
-        <div className={style.footerBody}></div>
+        <div className={style.footerBody}>
+          <div className={style.row}>
+            <div className={style.col_row_1}>
+              <div className={style.header_col_1}>
+                <div className={style.title}>
+                  <h1>T </h1>
+                  <h1>Devs</h1>
+                </div>
+
+                <p className={style.card_text}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Tenetur consectetur quod dolor possimus natus. Cum error odit,
+                  eveniet quas velit perspiciatis fugiat eius numquam.
+                  Architecto veritatis a velit ducimus maxime.
+                </p>
+              </div>
+            </div>
+            <div className={style.col_row_2}>
+              <div className={style.col}>
+                <h5>Services</h5>
+                <ul>
+                  <li>IT Consulting -</li>
+                  <li>Development</li>
+                  <li>Cloud</li>
+                  <li>DevOps & Support</li>
+                </ul>
+              </div>
+
+              <div className={style.col}>
+                <h5>Industries</h5>
+                <ul>
+                  <li>Finance</li>
+                  <li>Public Sector</li>
+                  <li>Smart Office</li>
+                  <li>Retail</li>
+                </ul>
+              </div>
+
+              <div className={style.col}>
+                <h5>Company</h5>
+                <ul>
+                  <li>About Us</li>
+                  <li>Blog</li>
+                  <li>Contact</li>
+                  <li>Join Us</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <hr />
+        </div>
       </div>
     </>
   );
