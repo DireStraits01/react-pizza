346d3dbf9f2a1fb496ec9671937df310c9a04b21
DireStraits01 <arsamak88@gmail.com>
2023-05-01 23:56:26 +0300

    prevetdefault log form button

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,13 +1,24 @@
 import style from './Authentication.module.css';
+import { useState } from 'react';
 
 function Authentication() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
-      <form action="#">
+      <form action="#" onSubmit={(e) => e.preventDefault}>
         <div className={style.authenForms}>
           <div>
             <label htmlFor="email"></label>
-            <input type="text" id="email" name="email" placeholder="email" />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="password"></label>
@@ -16,6 +27,11 @@ function Authentication() {
               id="password"
               name="password"
               placeholder="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                console.log(password);
+              }}
             />
           </div>
         </div>
